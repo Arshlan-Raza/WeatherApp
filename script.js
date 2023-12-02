26ecb641b0d9eae8e3c25cdafc183b79eab9d8ac
Arshlan <arshalanraja@gmail.com>
2023-12-02 07:29:39 +0530

    rendering the weather info

--- script.js
+++ script.js
@@ -25,6 +25,14 @@ function switchTab(clickedTab){
             grantAccessContainer.classList.remove('active');
             searchForm.classList.add('active');
          }
+         else{
+            // main pehle search wale tab pr tha , ab your weather tab visible krna h
+            searchForm.classList.remove('active');
+            userInfoContainerContainer.classList.remove('active');
+
+            // ab main your weather tab me aa gya to ab, so displaying the your weather tab 
+            getFromSessionStorage();
+         }
     }
 }
 
@@ -37,3 +45,47 @@ searchTab.addEventListener('click',() => {
     //pass clicked tab as input parameter
     switchTab(searchTab);
 });
+
+// checking if the coordinates are already present in session storage 
+function getFromSessionStorage(){
+    const localCoordinates = sessionStorage.getItem("user-coordinates");
+    if(!localCoordinates){
+        // if locaCoordinates are not present 
+        grantAccessContainer.classList.add('active');
+
+    }
+    else{
+        const coordinates = JSON.parse(localCoordinates);
+        fetchUserWeatherInfo(coordinates);   
+    }
+}
+
+async function fetchUserWeatherInfo(coordinates){
+    const {lat,lon} =coordinates;
+
+    // making the grant Access container visible 
+    grantAccessContainer.classList.remove("active");
+
+    // making the loader image visible 
+    loadingScreen.classList.add('active');
+
+    // calling the api which will fetch me the weather info 
+
+    try{
+
+    }
+    catch (err){
+
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`);
+
+        const data = await response.json();
+
+        // here we remove the loading screen as we got the coordinates 
+        loadingScreen.classList.remove('active');
+        userInfoContainer.classList.add('active');
+
+        // this function will fetch all the values we got after the api call and we will use the one which we need to show in our ui  
+        renderWeatherInfo(data);
+    }
+
+}
